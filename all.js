34469fa80def95a6a8acfa6758b2c59ff232a549
yehwansong <yehwanyensong@gmail.com>
2019-11-02 15:58:19 +0900

    add

--- all.js
+++ all.js
@@ -1,22 +1,23 @@
 $( document ).ready(function() {
 	$("#wrap").append("\
   		<div id='about_d'>\
-		    CavaCavaCavaCavaCavaCava\
-		    CavaCavaCavaCavaCavaCava\
-		    CavaCavaCavaCavaCavaCava\
-		    CavaCavaCavaCavaCavaCava\
-		    CavaCavaCavaCavaCavaCava\
-		    CavaCavaCavaCavaCavaCava\
-		    CavaCavaCavaCavaCavaCava\
-		    CavaCavaCavaCava\
-		</div>\
+  		<span>\
+		    Cava Discription\
+		</span>\
 		<div id='close'>close</div>\
+		</div>\
 	<div id='about'>About<br>Cava</div>\
 	");
 	$('#about').click(function(){
 		$('#about_d').addClass('about_d_open')
 		setTimeout(function(){
 			$('#about_d').addClass('about_d_open_color')
+		}, 500);
+	});
+	$('#close').click(function(){
+		$('#about_d').removeClass('about_d_open')
+		setTimeout(function(){
+			$('#about_d').removeClass('about_d_open_color')
 		}, 500);
 	})
 })
