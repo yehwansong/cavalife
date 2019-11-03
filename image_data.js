455dea7a86fda654233a0c8bf789351c0033aa0f
yehwansong <yehwanyensong@gmail.com>
2019-11-04 01:29:12 +0900

    add

--- image_data.js
+++ image_data.js
@@ -1,32 +1,6 @@
 
 $( document ).ready(function() {
     var hashnum = parseInt(window.location.hash.split('#')[1], 10)
-// if(hashnum == 3){
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/3_1_1.jpg")'></div><div style='background-image : url("artwork_image/3_1_2.jpg")'></div><div style='background-image : url("artwork_image/3_1_3.jpg")'></div><div style='background-image : url("artwork_image/3_1_4.jpg")'></div><div style='background-image : url("artwork_image/3_1_5.jpg")'></div><div style='background-image : url("artwork_image/3_1_6.jpg")'></div>	</div><div class='img img_2'><div style='background-image : url("artwork_image/3_2_1.jpg")'></div><div style='background-image : url("artwork_image/3_2_2.jpg")'></div><div style='background-image : url("artwork_image/3_2_3.jpg")'></div>	</div>`)
-// }
-// if(hashnum == 6){
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/6_1_1.jpg")'></div><div style='background-image : url("artwork_image/6_1_2.jpg")'></div><div style='background-image : url("artwork_image/6_1_3.jpg")'></div><div style='background-image : url("artwork_image/6_1_4.jpg")'></div><div style='background-image : url("artwork_image/6_1_5.jpg")'></div><div style='background-image : url("artwork_image/6_1_6.jpg")'></div><div style='background-image : url("artwork_image/6_1_7.jpg")'></div>	</div><div class='img img_2'><div style='background-image : url("artwork_image/6_2_1.jpg")'></div><div style='background-image : url("artwork_image/6_2_2.jpg")'></div><div style='background-image : url("artwork_image/6_2_3.jpg")'></div><div style='background-image : url("artwork_image/6_2_4.jpg")'></div>	</div>`)
-// }
-// if(hashnum == 8){
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/8_1_1.jpg")'></div><div style='background-image : url("artwork_image/8_1_2.jpg")'></div>	</div>`)
-// }
-// if(hashnum == 9){
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/9_1_1.jpg")'></div><div style='background-image : url("artwork_image/9_1_2.jpg")'></div><div style='background-image : url("artwork_image/9_1_3.jpg")'></div><div style='background-image : url("artwork_image/9_1_4.jpg")'></div><div style='background-image : url("artwork_image/9_1_5.jpg")'></div><div style='background-image : url("artwork_image/9_1_6.jpg")'></div><div style='background-image : url("artwork_image/9_1_7.jpg")'></div>	</div><div class='img'><div style='background-image : url("artwork_image/9_2_1.jpg")'></div><div style='background-image : url("artwork_image/9_2_2.jpg")'></div><div style='background-image : url("artwork_image/9_2_3.jpg")'></div><div style='background-image : url("artwork_image/9_2_4.jpg")'></div><div style='background-image : url("artwork_image/9_2_5.jpg")'></div>	</div><div class='img'><div style='background-image : url("artwork_image/9_3_1.jpg")'></div><div style='background-image : url("artwork_image/9_3_2.jpg")'></div>	</div><div class='img img_2'><div style='background-image : url("artwork_image/9_4_1.jpg")'></div><div style='background-image : url("artwork_image/9_4_2.jpg")'></div>	</div>`)
-// }
-// if(hashnum == 10){
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/10_1_1.jpg")'></div><div style='background-image : url("artwork_image/10_1_2.jpg")'></div><div style='background-image : url("artwork_image/10_1_3.jpg")'></div><div style='background-image : url("artwork_image/10_1_4.jpg")'></div>	</div><div class='img img_2'><div style='background-image : url("artwork_image/10_2_1.jpg")'></div><div style='background-image : url("artwork_image/10_2_2.jpg")'></div>	</div>`)
-// }
-// if(hashnum == 23){  
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/23_1_1.jpg")'></div><div style='background-image : url("artwork_image/23_1_2.jpg")'></div><div style='background-image : url("artwork_image/23_1_3.jpg")'></div><div style='background-image : url("artwork_image/23_1_4.jpg")'></div>	</div>`)
-// }
-// if(hashnum == 35){
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/35_1_1.jpg")'></div><div style='background-image : url("artwork_image/35_1_2.jpg")'></div><div style='background-image : url("artwork_image/35_1_3.jpg")'></div><div style='background-image : url("artwork_image/35_1_4.jpg")'></div><div style='background-image : url("artwork_image/35_1_5.jpg")'></div><div style='background-image : url("artwork_image/35_1_6.jpg")'></div><div style='background-image : url("artwork_image/35_1_7.jpg")'></div><div style='background-image : url("artwork_image/35_1_8.jpg")'></div><div style='background-image : url("artwork_image/35_1_9.jpg")'></div><div style='background-image : url("artwork_image/35_1_10.jpg")'></div><div style='background-image : url("artwork_image/35_1_11.jpg")'></div>	</div><div class='img img_2'><div style='background-image : url("artwork_image/35_2_1.jpg")'></div><div style='background-image : url("artwork_image/35_2_2.jpg")'></div><div style='background-image : url("artwork_image/35_2_3.jpg")'></div><div style='background-image : url("artwork_image/35_2_4.jpg")'></div><div style='background-image : url("artwork_image/35_2_5.jpg")'></div><div style='background-image : url("artwork_image/35_2_6.jpg")'></div><div style='background-image : url("artwork_image/35_2_7.jpg")'></div><div style='background-image : url("artwork_image/35_2_8.jpg")'></div><div style='background-image : url("artwork_image/35_2_9.jpg")'></div><div style='background-image : url("artwork_image/35_2_10.jpg")'></div><div style='background-image : url("artwork_image/35_2_11.jpg")'></div><div style='background-image : url("artwork_image/35_2_12.jpg")'></div><div style='background-image : url("artwork_image/35_2_13.jpg")'></div><div style='background-image : url("artwork_image/35_2_14.jpg")'></div><div style='background-image : url("artwork_image/35_2_15.jpg")'></div><div style='background-image : url("artwork_image/35_2_16.jpg")'></div><div style='background-image : url("artwork_image/35_2_17.jpg")'></div><div style='background-image : url("artwork_image/35_2_18.jpg")'></div><div style='background-image : url("artwork_image/35_2_19.jpg")'></div><div style='background-image : url("artwork_image/35_2_20.jpg")'></div><div style='background-image : url("artwork_image/35_2_21.jpg")'></div><div style='background-image : url("artwork_image/35_2_22.jpg")'></div><div style='background-image : url("artwork_image/35_2_23.jpg")'></div>	</div><div class='img img_3'><div style='background-image : url("artwork_image/35_3_1.jpg")'></div><div style='background-image : url("artwork_image/35_3_2.jpg")'></div>	</div>`)
-// }
-// if(hashnum == 37){
-// 	$('#imgbox').append(`<div class='img img_1'><div style='background-image : url("artwork_image/37_1_1.jpg")'></div><div style='background-image : url("artwork_image/37_1_2.jpg")'></div><div style='background-image : url("artwork_image/37_1_3.jpg")'></div><div style='background-image : url("artwork_image/37_1_4.jpg")'></div><div style='background-image : url("artwork_image/37_1_5.jpg")'></div><div style='background-image : url("artwork_image/37_1_6.jpg")'></div>	</div>`)
-// }
-
-
 
 if(hashnum == 1){
 	$('#imgbox').append(`
@@ -227,4 +201,280 @@ if(hashnum == 23){
 <div style='background-image : url("artwork_image/23_1_4.jpg")'></div>
 </div>
 `)}
+if(hashnum == 11){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/11_1_1.jpg")'></div>
+<div style='background-image : url("artwork_image/11_1_2.jpg")'></div>
+<div style='background-image : url("artwork_image/11_1_3.jpg")'></div>
+<div style='background-image : url("artwork_image/11_1_4.jpg")'></div>
+</div>
+`)}
+if(hashnum == 12){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/12_1_1.png")'></div>
+</div>
+`)}
+if(hashnum == 13){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/13_1_1.png")'></div>
+<div style='background-image : url("artwork_image/13_1_2.png")'></div>
+</div>
+`)}
+if(hashnum == 17){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/17_1_1.png")'></div>
+<div style='background-image : url("artwork_image/17_1_2.png")'></div>
+</div>
+`)}
+if(hashnum == 19){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/19_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 20){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/20_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 21){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/21_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 22){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/22_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 23){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/23_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 24){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/24_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 25){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/25_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 26){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/26_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 27){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/27_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 28){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/28_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 29){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/29_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 30){
+	$('#imgbox').append(`
+		<div class='img img_1'>
+<div style='background-image : url("artwork_image/30_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 31){
+	$('#imgbox').append(`
+	<div class='img img_1'>
+<div style='background-image : url("artwork_image/31_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 32){
+	$('#imgbox').append(`
+	<div class='img img_1'>
+<div style='background-image : url("artwork_image/32_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 33){
+	$('#imgbox').append(`
+	<div class='img img_1'>
+<div style='background-image : url("artwork_image/33_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 34){
+	$('#imgbox').append(`
+	<div class='img img_1'>
+<div style='background-image : url("artwork_image/34_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 35){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/35_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 36){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/36_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 37){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/37_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 38){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/38_1_1.png")'></div>
+</div>
+`)}
+if(hashnum == 39){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/39_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 40){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/40_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 41){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/41_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 42){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/42_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 43){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/43_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 44){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/44_1_1.png")'></div>
+</div>
+`)}
+if(hashnum == 45){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/45_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 46){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/46_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 47){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/47_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 48){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/48_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 49){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/49_1_1.jpg")'></div>
+</div>
+`)}
+if(hashnum == 50){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/50_1_1.jpg")'></div>
+<div style='background-image : url("artwork_image/50_1_2.jpg")'></div>
+<div style='background-image : url("artwork_image/50_1_3.jpg")'></div>
+<div style='background-image : url("artwork_image/50_1_4.jpg")'></div>
+<div style='background-image : url("artwork_image/50_1_5.jpg")'></div>
+</div>
+`)}
+if(hashnum == 51){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/51_1_1.jpg")'></div>
+<div style='background-image : url("artwork_image/51_1_2.jpg")'></div>
+<div style='background-image : url("artwork_image/51_1_3.jpg")'></div>
+<div style='background-image : url("artwork_image/51_1_4.jpg")'></div>
+<div style='background-image : url("artwork_image/51_1_5.jpg")'></div>
+</div>
+`)}
+if(hashnum == 53){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/53_1_1.jpg")'></div>
+<div style='background-image : url("artwork_image/53_1_2.jpg")'></div>
+<div style='background-image : url("artwork_image/53_1_3.jpg")'></div>
+<div style='background-image : url("artwork_image/53_1_4.jpg")'></div>
+<div style='background-image : url("artwork_image/53_1_5.jpg")'></div>
+</div>
+`)}
+if(hashnum == 54){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/54_1_1.jpg")'></div>
+<div style='background-image : url("artwork_image/54_1_2.jpg")'></div>
+</div>
+`)}
+if(hashnum == 55){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/55_1_1.jpg")'></div>
+<div style='background-image : url("artwork_image/55_1_2.jpg")'></div>
+<div style='background-image : url("artwork_image/55_1_3.jpg")'></div>
+<div style='background-image : url("artwork_image/55_1_4.jpg")'></div>
+<div style='background-image : url("artwork_image/55_1_5.jpg")'></div>
+<div style='background-image : url("artwork_image/55_1_6.jpg")'></div>
+</div>
+`)}
+if(hashnum == 56){
+$('#imgbox').append(`
+<div class='img img_1'>
+<div style='background-image : url("artwork_image/56_1_1.jpg")'></div>
+<div style='background-image : url("artwork_image/56_1_2.jpg")'></div>
+<div style='background-image : url("artwork_image/56_1_3.jpg")'></div>
+<div style='background-image : url("artwork_image/56_1_4.jpg")'></div>
+<div style='background-image : url("artwork_image/56_1_5.jpg")'></div>
+</div>
+`)}
+
+
+
 })
