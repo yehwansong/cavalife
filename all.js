7831474a532589da29faaf7d1572fda7972629fe
yehwansong <yehwanyensong@gmail.com>
2019-11-04 00:29:03 +0900

    add

--- all.js
+++ all.js
@@ -2,9 +2,10 @@ $( document ).ready(function() {
 	$("#wrap").append("\
   		<div id='about_d'>\
   		<span>\
-		    Cava Discription\
+			CAVA LIFE is a platform introducing artists of all kind and a select shop selling art pieces and products.<br><span class='kr'>CAVA LIFE는 동시대 다양한 장르의 작가를 소개하며 그들의 작품 및 제품을 판매하는 편집매장입니다.</span>\
 		</span>\
 		<div id='close'>close</div>\
+		<a href = 'intro.html' id='readmore'>readmore</a>\
 		</div>\
 	<div id='about'>About<br>Cava</div>\
 	");
