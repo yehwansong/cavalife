097a347dad76de6d50a0c804d95f7f114b631b7f
yehwansong <yehwanyensong@gmail.com>
2019-11-04 10:25:31 +0900

    add

--- all.js
+++ all.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
 	$("#wrap").append("\
   		<div id='about_d'>\
+      	<img src='../CAVA_logo.svg'>\
   		<span>\
 			CAVA LIFE is a platform introducing artists of all kind and a select shop selling art pieces and products.<br><span class='kr'>CAVA LIFE는 동시대 다양한 장르의 작가를 소개하며 그들의 작품 및 제품을 판매하는 편집매장입니다.</span>\
 		</span>\
-		<div id='close'>close</div>\
-		<a href = '../intro.html' id='readmore'>readmore</a>\
+		<div id='close'>Close</div>\
+		<a href = '../intro.html' id='readmore'>ABOUT<br>The Selects X CAVA<br>POP-UP STORE</a>\
 		</div>\
 	<div id='about'>About<br>Cava</div>\
 	");
